e8b380b189b63cda60fb599f0dc6b423df0a6a6c
Manda <mandaningthoujam84@gmail.com>
2023-11-21 17:34:56 +0530

    updated pages

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -13,15 +13,9 @@ const Hero = () => {
         width: '100%',
       }}
     >
-<<<<<<< HEAD
-      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
-        <h1 className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#f1ecec] font-[600] capitalize`}>
-          Best Collection for<br />Shopping Items
-=======
       <div className={` pt-48 ${styles.section} w-[90%] 800px:w-[60%]`}>
         <h1 className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#f1ecec] font-[600] capitalize p`}>
           Best Collection for<br />Fashion, E-Gadgets ...
->>>>>>> 1562b80827ab58624f7fe5fdfab8684f61c97c41
         </h1>
         <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#fffdfdf9]">
         Fashion Meets Innovation - Explore the Ultimate Collection of E-Gadgets and Trendy Styles.
