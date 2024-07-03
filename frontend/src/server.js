cd042e2f036bfbf3c1b4b42930b7ea9330afc061
Ayush Mutum <ayushmutum6@gmail.com>
2024-07-03 20:55:52 +0530

    fix

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -6,3 +6,4 @@ export const server = "https://ecom-shopping-website-g358.vercel.app/api/v2"
 export const backend_url = 'https://ecom-shopping-website-g358.vercel.app/'
 
 // 'http://localhost:8000/'
+
